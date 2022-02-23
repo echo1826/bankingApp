00f65cffb8face13aec568e05d271c06d91e67e6
Ethan C <ethan.cho1826@gmail.com>
2022-02-23 15:05:39 -0600

    checkings and savings accounts added, changed logic on quitting/go back options

--- bankingApp.java
+++ bankingApp.java
@@ -23,10 +23,10 @@ public class bankingApp {
         return data;
     }
 
-    private static float deposit(float depositMoney, float currentBalance) {
+    private static float deposit(float depositMoney, float currentBalance, String account) {
         float newBalance = currentBalance + depositMoney;
         try {
-            FileWriter myWriter = new FileWriter("balance.txt");
+            FileWriter myWriter = new FileWriter(account);
             String balanceToBeWritten = Float.toString(newBalance);
             myWriter.write(balanceToBeWritten);
             myWriter.close();
@@ -36,10 +36,10 @@ public class bankingApp {
         return newBalance;
     }
 
-    private static float withdraw(float withdrawMoney, float currentBalance) {
+    private static float withdraw(float withdrawMoney, float currentBalance, String account) {
         float newBalance = currentBalance - withdrawMoney;
         try{
-            FileWriter myWriter = new FileWriter("balance.txt");
+            FileWriter myWriter = new FileWriter(account);
             String balanceToBeWritten = Float.toString(newBalance);
             myWriter.write(balanceToBeWritten);
             myWriter.close();
@@ -47,30 +47,64 @@ public class bankingApp {
             e.printStackTrace();
         }
         return newBalance;
+    }
+
+    private static String accountChoice() {
+        Scanner scannerObj = new Scanner(System.in);
+        while(true) {
+            System.out.println("Please enter what account you would like to make a transaction in: ");
+            System.out.println("1. Checkings\n2. Savings\n3. Quit");
+            int accountChoice = scannerObj.nextInt();
+            switch(accountChoice) {
+                case 1: {
+                    try{
+                        File fileObject = new File("checkings.txt");
+                        if(fileObject.createNewFile()) {
+                            FileWriter writer = new FileWriter("checkings.txt");
+                            writer.write("0.00");
+                            writer.close();
+                            return "checkings.txt";
+                        }else {
+                            return "checkings.txt";
+                        }
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                    
+                }
+                case 2: {
+                    try {
+                        File fileObject = new File("savings.txt");
+                        if(fileObject.createNewFile()) {
+                            FileWriter writer = new FileWriter("savings.txt");
+                            writer.write("0.00");
+                            writer.close();
+                            return "savings.txt";
+                        }else {
+                            return "savings.txt";
+                        }
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                case 3: {
+                    scannerObj.close();
+                    System.exit(1);
+                }
+                default: {
+                    System.out.println("Not a valid choice, please try again.");
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner scannerObj = new Scanner(System.in);
-        float balance = 0f;
-        try {
-            File myFileObject = new File("balance.txt");
-            if (myFileObject.createNewFile()) {
-                System.out.println("File created: " + myFileObject.getName());
-                FileWriter myWriter = new FileWriter("balance.txt");
-                String balanceToBeWritten = Float.toString(balance);
-                myWriter.write(balanceToBeWritten);
-                myWriter.close();
-            } else {
-                System.out.println("File already exists.");
-                balance = Float.parseFloat(readFile("balance.txt"));
-            }
-        } catch (IOException e) {
-            System.out.println("Error occured");
-            e.printStackTrace();
-        }
+        String account = accountChoice();
+        float balance = Float.parseFloat(readFile(account));
         while (true) {
             System.out.println("Please enter what you would like to do (1 - 3): \n");
-            System.out.println("1. Check Balance\n2. Deposit\n3. Withdraw\n4. Quit");
+            System.out.println("1. Check Balance\n2. Deposit\n3. Withdraw\n4. Go Back");
             int userChoice = scannerObj.nextInt();
             switch (userChoice) {
                 case 1: {
@@ -80,22 +114,23 @@ public class bankingApp {
                 case 2: {
                     System.out.println("How much would you like to deposit?: ");
                     float moneyToDeposit = scannerObj.nextFloat();
-                    balance = Float.parseFloat(readFile("balance.txt"));
-                    balance = deposit(moneyToDeposit, balance);
+                    balance = Float.parseFloat(readFile(account));
+                    balance = deposit(moneyToDeposit, balance, account);
                     System.out.println(String.format("%.2f", balance));
                     break;
                 }
                 case 3: {
                     System.out.println("How much would you like to withdraw?: ");
                     float moneyToWithdraw = scannerObj.nextFloat();
-                    balance = Float.parseFloat(readFile("balance.txt"));
-                    balance = withdraw(moneyToWithdraw, balance);
+                    balance = Float.parseFloat(readFile(account));
+                    balance = withdraw(moneyToWithdraw, balance, account);
                     System.out.println(String.format("%.2f", balance));
                     break;
                 }
                 case 4: {
-                    scannerObj.close();
-                    System.exit(1);
+                    // scannerObj.close();
+                    account = accountChoice();
+                    balance = Float.parseFloat(readFile(account));
                 }
                 default: {
                     System.out.println("Not a valid option, please choose between 1, 2, 3, 4.");
